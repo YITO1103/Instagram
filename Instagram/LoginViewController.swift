c02cf104b0747119addada1c200c8ce580388972
YITO1103 <yuta.j.1103@gmail.com>
2020-01-14 19:59:02 +0900

    0114 05

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -7,8 +7,24 @@
 //
 
 import UIKit
+import Firebase
+
+
 
 class LoginViewController: UIViewController {
+
+    @IBOutlet weak var mailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    
+    // ログインボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleLoginButton(_ sender: Any) {
+    }
+    // アカウント作成ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleCreateAccountButton(_ sender: Any) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
