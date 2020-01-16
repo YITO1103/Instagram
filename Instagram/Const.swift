074c49be07d405305b2a3b7409fcd15963ad0824
YITO1103 <yuta.j.1103@gmail.com>
2020-01-16 18:33:31 +0900

    0116 04

---
+++ Instagram/Const.swift
@@ -0,0 +1,17 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by user1 on 2020/01/16.
+//  Copyright © 2020 yutaka.ito4. All rights reserved.
+//
+
+import Foundation
+
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
+
+
